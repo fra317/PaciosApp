ee87a32092b547c1ad00cbbb247d090e41796b9f
Francesco Prisco <francesco.prisco070901@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ PaciosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaciosApp
+//
+//  Created by Francesco Prisco on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaciosApp/PaciosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaciosAppApp.swift
+//  PaciosApp
+//
+//  Created by Francesco Prisco on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PaciosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
